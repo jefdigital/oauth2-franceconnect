c6195117bbc8211aaade04bbe62f6dab7e4fe61c
Jean-François MAGNIER <hello@jefdigital.fr>
2017-12-13 14:28:10 +0400

    Resource owner created
    Resource Owner completed with fields available in profile scope
    
    Better description in README file

--- src/Provider/FranceConnectResourceOwner.php
+++ src/Provider/FranceConnectResourceOwner.php
@@ -22,4 +22,85 @@ class FranceConnectResourceOwner implements ResourceOwnerInterface
     {
         $this->response = $response;
     }
+
+    /**
+     * Get resource owner id
+     *
+     * @return string|null
+     */
+    public function getId()
+    {
+        return $this->response['sub'] ?: null;
+    }
+
+    /**
+     * Return all of the owner details available as an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->response;
+    }
+
+    /**
+     * Get resource owner email
+     *
+     * @return string|null
+     */
+    public function getEmail()
+    {
+        return $this->response['email'] ?: null;
+    }
+
+    /**
+     * Get resource owner gender
+     *
+     * @return string|null
+     */
+    public function getGender()
+    {
+        return $this->response['gender'] ?: null;
+    }
+
+    /**
+     * Get resource owner given name
+     *
+     * @return string|null
+     */
+    public function getGivenName()
+    {
+        return $this->response['given_name'] ?: null;
+    }
+
+    /**
+     * Get resource owner first name
+     *
+     * @return string|null
+     */
+    public function getFirstName()
+    {
+        return $this->response['first_name'] ?: null;
+    }
+
+    /**
+     * Get resource owner birth date
+     *
+     * @return string|null
+     */
+    public function getBirthDate()
+    {
+        return $this->response['birthdate'] ?: null;
+    }
+
+    /**
+     * Get resource owner birth place
+     *
+     * @return string|null
+     */
+    public function getBirthPlace()
+    {
+        return $this->response['birthplace'] ?: null;
+    }
+
 }
